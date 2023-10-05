ace44e66069921581e3fcd952c7a33b3d79948ee
Dnyandeep Dhok <83214724+Dnyandeep2104@users.noreply.github.com>
2023-10-04 18:59:53 -0700

    Added name Dnyandeep Ravindra Dhok to IndexModel Comment in Index.cshtml.cs

--- 5110FA23-master/src/Pages/Index.cshtml.cs
+++ 5110FA23-master/src/Pages/Index.cshtml.cs
@@ -9,7 +9,7 @@ using ContosoCrafts.WebSite.Services;
 namespace ContosoCrafts.WebSite.Pages
 {
     /// <summary>
-    /// Mike Koenig
+    /// Dnyandeep Ravindra Dhok
     /// </summary>
     public class IndexModel : PageModel
     {
