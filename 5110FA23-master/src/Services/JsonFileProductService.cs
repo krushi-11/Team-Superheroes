1aec7c57813da11bf8ce9a4fb8efc6a33db88a2b
Dnyandeep Dhok <83214724+Dnyandeep2104@users.noreply.github.com>
2023-10-23 00:59:44 -0700

    Revert "Modified AddRating Service in JsonFileProductServices.cs"
    This reverts commit 8d668e1d77955308b0d9c0454abecc5c4269221c.

--- 5110FA23-master/src/Services/JsonFileProductService.cs
+++ 5110FA23-master/src/Services/JsonFileProductService.cs
@@ -1,8 +1,6 @@
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net.Http.Json;
 using System.Text.Json;
 
 using ContosoCrafts.WebSite.Models;
@@ -37,15 +35,12 @@ namespace ContosoCrafts.WebSite.Services
             }
         }
 
-        // Changed return type to boolean so that we can acknowledge the output in the unit test
-        public bool AddRating(string productId, int rating)
+        public void AddRating(string productId, int rating)
         {
             var products = GetProducts();
-            var check = true;
 
             if(products.First(x => x.Id == productId).Ratings == null)
             {
-                check = false;
                 products.First(x => x.Id == productId).Ratings = new int[] { rating };
             }
             else
@@ -53,7 +48,6 @@ namespace ContosoCrafts.WebSite.Services
                 var ratings = products.First(x => x.Id == productId).Ratings.ToList();
                 ratings.Add(rating);
                 products.First(x => x.Id == productId).Ratings = ratings.ToArray();
-                check = false;
             }
 
             using(var outputStream = File.OpenWrite(JsonFileName))
@@ -67,7 +61,6 @@ namespace ContosoCrafts.WebSite.Services
                     products
                 );
             }
-            return check;
         }
     }
 }
