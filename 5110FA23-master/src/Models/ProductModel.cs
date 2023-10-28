f40ff079b639870be9ba001f5c62c94cc7ec736d
Kode Sai Vikshit <kodevikshit2000@gmail.com>
2023-10-28 13:00:54 -0700

    Implemented Model for Price, Stock in ProductModel.cs file

--- 5110FA23-master/src/Models/ProductModel.cs
+++ 5110FA23-master/src/Models/ProductModel.cs
@@ -13,6 +13,8 @@ namespace ContosoCrafts.WebSite.Models
         public string Url { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public float Price { get; set; } // Price of items
+        public int Stock { get; set; } // Number of items left in stock
         public int[] Ratings { get; set; }
 
         public override string ToString() => JsonSerializer.Serialize<ProductModel>(this);
