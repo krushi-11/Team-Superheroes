2881fecb7597856edc272db2387f1dee7bfd882f
Dnyandeep Dhok <83214724+Dnyandeep2104@users.noreply.github.com>
2023-10-29 14:39:43 -0700

    Created an OnPost request to Delete  product data in Delete.cshtml.cs

---
+++ 5110FA23-master/src/Pages/Product/Delete.cshtml.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ContosoCrafts.WebSite.Services;
+using ContosoCrafts.WebSite.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoCrafts.WebSite.Pages.Product
+{
+    public class DeleteModel : PageModel
+    {
+        public JsonFileProductService ProductService { get; }
+
+        /// <summary>
+        /// Defualt Construtor
+        /// </summary>
+        public DeleteModel(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
+
+        [BindProperty]
+        public ProductModel Product { get; set; }
+
+        public void OnGet(string id)
+        {
+            Product = ProductService.GetProducts().FirstOrDefault(m => m.Id.Equals(id));
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                // Handle the case where ModelState is not valid
+                return Page();
+            }
+            // Update the product title using the service
+            ProductService.DeleteData(Product.Id);
+
+            // Redirect to a confirmation page or a product list page
+            return RedirectToPage("./Index");
+
+        }
+
+    }
+}
