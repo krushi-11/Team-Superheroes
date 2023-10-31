77381ef9f9e7f91259bf298b68be73b800bac593
Dnyandeep Dhok <83214724+Dnyandeep2104@users.noreply.github.com>
2023-10-30 19:11:45 -0700

    Created a file named Delete.cshtml.Tests.cs

---
+++ 5110FA23-master/UnitTests/Pages/Product/Delete.cshtml.Tests.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using ContosoCrafts.WebSite.Pages.Product;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+using NUnit.Framework;
+using ContosoCrafts.WebSite.Models;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+
+namespace UnitTests.Pages.Product.Delete
+{
+
+}
+
+
+
+
+    
