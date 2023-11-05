7f21ef06d30b165708bb8f470fa8c4fdcedcd571
Kode Sai Vikshit <kodevikshit2000@gmail.com>
2023-11-04 19:42:19 -0700

    Added Comments in ProductModel.cs file

--- 5110FA23-master/src/Models/ProductModel.cs
+++ 5110FA23-master/src/Models/ProductModel.cs
@@ -5,20 +5,18 @@ namespace ContosoCrafts.WebSite.Models
 {
     public class ProductModel
     {
-        public string Id { get; set; }
-        public string Maker { get; set; }
+        public string Id { get; set; } // IdModel of the Product
+        public string Maker { get; set; } // MakerModel of the Product
         
         [JsonPropertyName("img")]
-        public string Image { get; set; }
-        public string Url { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public float Price { get; set; } // Price of items
-        public int Stock { get; set; } // Number of items left in stock
-        public int[] Ratings { get; set; }
+        public string Image { get; set; } // ImageModel of the Product
+        public string Url { get; set; } // UrlModel of the Product
+        public string Title { get; set; } // TitleModel of the Product
+        public string Description { get; set; } // DescriptionModel of the Product
+        public float Price { get; set; } // PriceModel of the Product
+        public int Stock { get; set; } // StockModel of the Product
+        public int[] Ratings { get; set; } // RatingsModel of the Product
 
         public override string ToString() => JsonSerializer.Serialize<ProductModel>(this);
-
- 
     }
 }
