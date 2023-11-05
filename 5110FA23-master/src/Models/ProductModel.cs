d66b2cd35db6d69eeadc6147659a5169833df235
Kode Sai Vikshit <kodevikshit2000@gmail.com>
2023-11-04 19:49:33 -0700

    Added Range attributes to ProductModels in ProductModel.cs file

--- 5110FA23-master/src/Models/ProductModel.cs
+++ 5110FA23-master/src/Models/ProductModel.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -6,15 +7,25 @@ namespace ContosoCrafts.WebSite.Models
     public class ProductModel
     {
         public string Id { get; set; } // IdModel of the Product
+
         public string Maker { get; set; } // MakerModel of the Product
         
         [JsonPropertyName("img")]
         public string Image { get; set; } // ImageModel of the Product
+
         public string Url { get; set; } // UrlModel of the Product
+
+        [MaxLength(50, ErrorMessage = "Title must not exceed 50 characters.")]
         public string Title { get; set; } // TitleModel of the Product
+
         public string Description { get; set; } // DescriptionModel of the Product
+
+        [Range(0, float.MaxValue, ErrorMessage = "Price should be only Positive.")]
         public float Price { get; set; } // PriceModel of the Product
+
+        [Range(0, int.MaxValue, ErrorMessage = "Price must be non-negative.")]
         public int Stock { get; set; } // StockModel of the Product
+
         public int[] Ratings { get; set; } // RatingsModel of the Product
 
         public override string ToString() => JsonSerializer.Serialize<ProductModel>(this);
