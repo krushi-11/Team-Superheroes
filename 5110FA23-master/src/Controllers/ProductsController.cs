0c2db43c0f8bc3ba5955c3a5a1c798f49a444b33
Kode Sai Vikshit <kodevikshit2000@gmail.com>
2023-11-04 19:41:43 -0700

    Added Comments to Methods in ProductController.cs file

--- 5110FA23-master/src/Controllers/ProductsController.cs
+++ 5110FA23-master/src/Controllers/ProductsController.cs
@@ -1,14 +1,14 @@
 using System.Collections.Generic;
-
 using Microsoft.AspNetCore.Mvc;
-
 using ContosoCrafts.WebSite.Models;
 using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite.Controllers
 {
+
     [ApiController]
     [Route("[controller]")]
+
     public class ProductsController : ControllerBase
     {
         public ProductsController(JsonFileProductService productService)
@@ -18,12 +18,14 @@ namespace ContosoCrafts.WebSite.Controllers
 
         public JsonFileProductService ProductService { get; }
 
+        // Get all Products from Json File
         [HttpGet]
         public IEnumerable<ProductModel> Get()
         {
             return ProductService.GetProducts();
         }
 
+        // Requesting Rating from User
         [HttpPatch]
         public ActionResult Patch([FromBody] RatingRequest request)
         {
@@ -32,6 +34,7 @@ namespace ContosoCrafts.WebSite.Controllers
             return Ok();
         }
 
+        // ProductModel To Request Rating from User
         public class RatingRequest
         {
             public string ProductId { get; set; }
