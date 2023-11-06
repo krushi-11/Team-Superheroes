fc318e7bf8ff75145eae553c95668cdea28a6314
Dnyandeep Dhok <83214724+Dnyandeep2104@users.noreply.github.com>
2023-11-06 12:50:51 -0800

    Removed unnecessary references from Update.cshtml.cs

--- 5110FA23-master/src/Pages/Product/Update.cshtml.cs
+++ 5110FA23-master/src/Pages/Product/Update.cshtml.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using ContosoCrafts.WebSite.Services;
 using ContosoCrafts.WebSite.Models;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace ContosoCrafts.WebSite.Pages.Product
 {
