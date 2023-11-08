da6ec296c951adae9bab679a8d258b31e53b5b30
Krushikesh Thotange <kthotange@seattleu.edu>
2023-11-07 19:26:38 -0800

    Added comments for variables in Read.cshtml.Tests.cs

--- 5110FA23-master/UnitTests/Pages/Product/Read.cshtml.Tests.cs
+++ 5110FA23-master/UnitTests/Pages/Product/Read.cshtml.Tests.cs
@@ -53,11 +53,13 @@ namespace UnitTests.Pages.Product.Read
 
             // Setting up a mock WebHostEnvironment
 
+            ///creating variable mockWebHostEnvironment
             var mockWebHostEnvironment = new Mock<IWebHostEnvironment>();
             mockWebHostEnvironment.Setup(m => m.EnvironmentName).Returns("Hosting:UnitTestEnvironment");
             mockWebHostEnvironment.Setup(m => m.WebRootPath).Returns("../../../../src/bin/Debug/net7.0/wwwroot");
             mockWebHostEnvironment.Setup(m => m.ContentRootPath).Returns("./data/");
 
+            ///creating variable MockLoggerDirect
             var MockLoggerDirect = Mock.Of<ILogger<IndexModel>>();
             JsonFileProductService productService;
 
