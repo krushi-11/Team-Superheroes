68f366584607b2162684bcf7f98f5a05602c1b33
Aryan Khetarpal <akhetarpal@seattleu.edu>
2023-11-12 05:22:37 +0530

    Added /// comments to Index.cshtml.cs

--- 5110FA23-master/src/Pages/Index.cshtml.cs
+++ 5110FA23-master/src/Pages/Index.cshtml.cs
@@ -8,28 +8,28 @@ using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite.Pages
 {
-    //Index page model for homepage
+    ///Index page model for homepage
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
 
-        //Constructor for Index model
+        ///Constructor for Index model
         public IndexModel(ILogger<IndexModel> logger,
             JsonFileProductService productService)
         {
             _logger = logger;
             ProductService = productService;
         }
-        //Property to access JsonFileProductService
+        ///Property to access JsonFileProductService
         public JsonFileProductService ProductService { get; }
         
-        // Property to store a collection of product models
+        /// Property to store a collection of product models
         public IEnumerable<ProductModel> Products { get; private set; }
 
-        // OnGet method called when the page is requested
+        /// OnGet method called when the page is requested
         public void OnGet()
         {
-            // Get a list of products from the ProductService
+            /// Get a list of products from the ProductService
             Products = ProductService.GetProducts();
         }
     }
