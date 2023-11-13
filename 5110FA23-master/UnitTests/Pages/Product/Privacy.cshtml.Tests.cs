bd8cdb1c0d8cbbabbe480460394cf98e01ae8b2a
Aryan Khetarpal <akhetarpal@seattleu.edu>
2023-11-13 05:56:57 +0530

    Created Privacy.cshtml.Tests.cs file to unit test Privacy page

---
+++ 5110FA23-master/UnitTests/Pages/Product/Privacy.cshtml.Tests.cs
@@ -0,0 +1,27 @@
+﻿using ContosoCrafts.WebSite.Pages;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+
+namespace UnitTests.Pages
+{
+    [TestFixture]
+    public class PrivacyModelTests
+    {
+        [Test]
+        public void OnGet_ShouldReturnPageResult()
+        {
+            // Arrange
+            var loggerMock = new Mock<ILogger<PrivacyModel>>();
+            var privacyModel = new PrivacyModel(loggerMock.Object);
+
+            // Act
+            privacyModel.OnGet();
+
+            // Assert
+            var result = privacyModel.Page();
+            Assert.IsInstanceOf<PageResult>(result);
+        }
+    }
+}
