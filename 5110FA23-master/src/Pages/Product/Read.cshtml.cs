b9309a6f271f3add2efc1802f2d170b7000ffa6c
Kode Sai Vikshit <kodevikshit2000@gmail.com>
2023-11-14 23:43:59 -0800

    Wrote appropriate comments in Read.cshtml.cs file

--- 5110FA23-master/src/Pages/Product/Read.cshtml.cs
+++ 5110FA23-master/src/Pages/Product/Read.cshtml.cs
@@ -13,9 +13,9 @@ namespace ContosoCrafts.WebSite.Pages.Product
         /// </summary>
         public JsonFileProductService ProductService { get; }
 
-        
+        /// <summary>
         /// Defualt Construtor
-        
+        /// </summary>
         /// <param name="logger"></param>
         /// <param name="productService"></param>
         public ReadModel(JsonFileProductService productService)
@@ -28,9 +28,9 @@ namespace ContosoCrafts.WebSite.Pages.Product
         /// </summary>
         public ProductModel Product;
 
-        
+        /// <summary>
         /// REST Get request
-        
+        /// </summary>
         /// <param name="id"></param>
         public void OnGet(string id) /// OnGet Request to Get Products by id
         {
