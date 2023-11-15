70e8959f01f00f1306baf15f7d64c51d2ae9addc
Dnyandeep Dhok <83214724+Dnyandeep2104@users.noreply.github.com>
2023-11-14 22:48:40 -0800

    Wrote a UT to Add To Cart Button Should Add Product To Shopping Cart in ProductListTests.cs

--- 5110FA23-master/UnitTests/Components/ProductListTests.cs
+++ 5110FA23-master/UnitTests/Components/ProductListTests.cs
@@ -8,31 +8,28 @@ using System.Collections.Generic;
 using System.Linq;
 using ContosoCrafts.WebSite.Components;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
 
 namespace UnitTests.Components
 {
-    public class ProductListTests
+    public class ProductListTests: Bunit.TestContext
     {
         [Test]
         public void AddToCartButtonShouldAddProductToShoppingCart()
         {
-            // Arrange
-            using var ctx = new Bunit.TestContext();
-
-            // Register your services, you may need to customize this based on your setup
-            ctx.Services.AddSingleton<JsonFileProductService>();
+           
+            // To register the JsonFileProductServices
+            Services.AddSingleton<JsonFileProductService>(TestHelper.ProductService);
 
             // Render the component
-            var cut = ctx.RenderComponent<ProductList>();
+            var cut = RenderComponent<ProductList>();
 
             // Act: Click on the Add to Cart button
             cut.Find(".addtocart").Click();
 
             // Assert: Verify that the product is added to the shopping cart
-            var shoppingCart = cut.Find("ul");
-            Assert.IsTrue(shoppingCart.TextContent.Contains("AddedProductTitle"));
+            var shoppingCart = cut.Find("#addedToCart");
+            Assert.IsTrue(shoppingCart.TextContent.Contains("Added to Cart"));
         }
-
-        // Add more tests as needed
     }
 }
