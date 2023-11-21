2b54f27d430c707e745818c50430019b03fb1b6d
Dnyandeep Dhok <83214724+Dnyandeep2104@users.noreply.github.com>
2023-11-21 02:10:53 -0800

    Wrote Unit Test for SelectProduct in ProductListTests.cs

--- 5110FA23-master/UnitTests/Components/ProductListTests.cs
+++ 5110FA23-master/UnitTests/Components/ProductListTests.cs
@@ -3,12 +3,38 @@ using NUnit.Framework;
 using Bunit;
 using ContosoCrafts.WebSite.Components;
 using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using System;
+using System.IO;
 
 namespace UnitTests.Components
 {
-    /// <summary>
-    /// Unit tests for the ProductList component
-    /// </summary>
-    
-    
+
+    public class ProductListTests : Bunit.TestContext
+    {
+        [Test]
+        public void SelectProduct_InValidId_returns_false()
+        {
+            Services.AddSingleton<JsonFileProductService>(TestHelper.ProductService);
+            var id = "MoreInfoButton_t-challa";
+
+            var page = RenderComponent<ProductList>();
+
+            var buttonList = page.FindAll("Button");
+
+            var button = buttonList.First(m => m.OuterHtml.Contains(id));
+
+            button.Click();
+
+            var pageMarkup = page.Markup;
+            Assert.AreEqual(false, pageMarkup.Contains("Testing"));
+
+        }
+
+
+    }
+
+
+
+
 }
