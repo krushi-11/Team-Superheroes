5d119984a0d9e8cd5b601e23ddc28db738db9073
Dnyandeep Dhok <83214724+Dnyandeep2104@users.noreply.github.com>
2023-11-28 14:52:45 -0800

    Created a new page for unit testing of PageNotFound.cshtml.cs

---
+++ 5110FA23-master/UnitTests/Pages/Shared/PageNotFound.cshtml.Tests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using SuperHeroes.WebSite.Pages;
+using System.Diagnostics;
+
+namespace SuperHeroes.WebSite.Tests.Pages
+{
+    [TestFixture]
+    public class PageNotFoundModelTests
+    {
+        private Mock<ILogger<ErrorModel>> _loggerMock;
+        private PageNotFoundModel _pageModel;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Arrange: Create a mock logger for the page model
+            _loggerMock = new Mock<ILogger<ErrorModel>>();
+
+            // Act: Instantiate PageNotFoundModel with the mock logger
+            _pageModel = new PageNotFoundModel(_loggerMock.Object);
+        }
+
+        
+    }
+}
