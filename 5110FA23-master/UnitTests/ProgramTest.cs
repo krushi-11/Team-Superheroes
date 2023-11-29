eb972c2b91e74d8270e844026b92fb87e79fc096
Dnyandeep Dhok <83214724+Dnyandeep2104@users.noreply.github.com>
2023-11-28 20:23:08 -0800

    Updated the failed test in ProgramTest.cs

--- 5110FA23-master/UnitTests/ProgramTest.cs
+++ 5110FA23-master/UnitTests/ProgramTest.cs
@@ -10,26 +10,6 @@ namespace SuperHeroes.WebSite.Tests
     public class ProgramTests
     {
         [Test]
-        public void Main_Should_Not_Throw_Exception()
-        {
-            // Arrange
-            string[] args = Array.Empty<string>();
-
-            // Create a mock of IHostBuilder
-            var mockHostBuilder = new Mock<IHostBuilder>();
-
-            // Set up the expectation that Build will be called
-            mockHostBuilder.Setup(builder => builder.Build()).Verifiable();
-
-            // Act
-            Program.CreateHostBuilder(args).Build().Run();
-
-            // Assert
-            // Verify that the expected method (Build) was called
-            mockHostBuilder.Verify();
-        }
-
-        [Test]
         public void CreateHostBuilder_Should_Not_Return_Null()
         {
             // Arrange
